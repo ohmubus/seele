23f3fc34ce734c7abfda85aceb937c5a2f53b70e
enlore <n.e.lorenson@gmail.com>
2015-12-11 14:44:25 -0600

    Commentary, use method

--- src/seele.js
+++ src/seele.js
@@ -1,14 +1,17 @@
 ;(function SEELE (_this) {
-    var _hiddenCouncil = {}
-      , _agents = {}
+    var _hiddenCouncil = {} // plugin factories
+      , _agents = {}        // plugin instances
 
+    // this gets passed into each plugin. the plugin can then call it to
+    // have seele destroy it at the end of its lifecycle
     function _cyanideTooth (_agent) {
         var _i = _agents.indexOf(_agent)
         _agents.splice(_i, 1)
     }
 
-    var seele = {}
+    var seele = {} // namespace
 
+    // register a plugin by unique name
     seele.register = function (name, member) {
         if (_hiddenCouncil.hasOwnProperty(name))
             throw new Error("Memebers of SEELE cannot be replaced (I already have a plugin by that name)")
@@ -16,11 +19,12 @@
         _hiddenCouncil[name] = member
     }
 
-    seele.enact = function (name, interface) {
+    // elect to use a registered plugin by name and pass the interface object
+    seele.use = function (name, interface) {
         if (!Array.isArray(_agents[name]))
             _agents[name] = []
         _agantes[name].push(_hiddenCouncil[name](interface, this._cyanideTooth))
     }
 
-    _this.seele = seele
+    _this.seele = seele // bind to window (or whatever)
 })(this)
