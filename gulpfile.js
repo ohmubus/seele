fb15e989c766522cfd35a39ce8b920ca412baa39
enlore <n.e.lorenson@gmail.com>
2015-12-11 14:17:33 -0600

    Build proc

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const g     = require("gulp-load-plugins")()
+    , gulp  = require("gulp")
+
+const paths = {
+    src: "index.js",
+    dest: "dist"
+}
+
+gulp.task("watch", () => {
+    gulp.watch(paths.src, ["copy"])
+})
+
+gulp.task("copy", () => {
+    gulp.src(paths.src).pipe(gulp.dest(paths.dest))
+})
+
+const tasks = [
+    "copy",
+    "watch"
+]
+
+gulp.task("default", tasks)
